de0e83b806daff17d4ce493c5ffca6b795591940
kudiyarohit <2022csrohit12759@poornima.edu.in>
2025-04-27 23:57:07 +0530

    Add GitHub Actions CI/CD workflow

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask App with CI/CD!"
+    return "Hello from Flask App with CI/CD! My name is Rohit"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
